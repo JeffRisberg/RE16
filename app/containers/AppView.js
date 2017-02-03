60568e9b556490195ea906815d8faffb43e07e1a
JeffRisberg <jrisberg@aol.com>
2017-02-03 14:56:12 -0800

    improved menu structure

--- app/containers/AppView.js
+++ app/containers/AppView.js
@@ -2,21 +2,17 @@ import React from 'react';
 
 import { Link } from 'react-router'
 
-import body from '../styles/re16.scss';
-
 class AppView extends React.Component {
 
     render() {
         return (
-            <div className={body}>
-                <img src="/images/logo.gif"/>
-                <br/>
-                <Link to="/">Home</Link>
-                <br/>
-                <Link to="/charities">Charities</Link>
-                <br/>
-                <Link to="/donations">Donations</Link>
-                <br/>
+            <div>
+                <ul className='navlist'>
+                    <li><img src="/images/logo.gif"/></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/charities">Charities</Link></li>
+                    <li><Link to="/donations">Donations</Link></li>
+                </ul>
 
                 <div>
                     {this.props.children}
